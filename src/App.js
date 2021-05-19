a180f38a3c3396c2c7efc65fa5a37e9b51574581
David <david17895@gmail.com>
2021-05-19 13:57:22 +0300

    home header styling and HTML

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import './App.scss';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className='header__content'>
+        <div className='header__content__text'>
+          <h1>Welcome to the cliché weather app</h1>
+          <h3>It's just another weather app.. really</h3>
+        </div>
+      </div>
+    </header>
+  );
+}
